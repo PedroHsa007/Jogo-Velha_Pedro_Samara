2e26c7be1bb81fd75eb6f8b53cff1370be08b142
202120423 <202120423@TPCP04LAB1010>
2023-08-30 19:03:52 -0300

    Adicionar arquivos de projeto.

---
+++ Jogo-Velha/JogoDaVelha.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_Velha
+{
+    public class Tabuleiro
+    {
+        private char[,] board = new char[3, 3];
+        private char jogadorAtual = 'X';
+        public Tabuleiro()
+        {
+            IniciarTabuleiro();
+        }
+        private void IniciarTabuleiro()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = ' ';
+                }
+            }
+        }
+        public bool FazerJogada(int linha, int coluna)
+        {
+            if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2 || board[linha, coluna] != ' ')
+            {
+                return false;
+            }
+            board[linha, coluna] = jogadorAtual;
+            jogadorAtual = (jogadorAtual == 'X') ? 'O' : 'X';
+            return true;
+        }
+        public bool VerificarVitoria()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] != ' ' && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    return true; // Linha i venceu
+                }
+                if (board[0, i] != ' ' && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    return true; // Coluna i venceu
+                }
+            }
+            if (board[0, 0] != ' ' && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                return true; // Diagonal principal venceu
+            }
+            if (board[0, 2] != ' ' && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                return true; // Diagonal secundária venceu
+            }
+            return false; // Nenhum jogador venceu ainda
+        }
+        public bool VerificarEmpate()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                    {
+                        return false; // Ainda há espaços vazios no tabuleiro
+                    }
+                }
+            }
+            return true; // Tabuleiro está cheio, é empate
+        }
+        public char[,] ObterTabuleiro()
+        {
+            return board;
+        }
+        public char ObterJogadorAtual()
+        {
+            return jogadorAtual;
+        }
+    }
+}
---
+++ Jogo-Velha/Program.cs
@@ -0,0 +1,56 @@
+﻿using Jogo_Velha;
+using System;
+namespace JogoDaVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tabuleiro = new Tabuleiro();
+            while (!tabuleiro.VerificarVitoria() && !tabuleiro.VerificarEmpate())
+            {
+                char jogadorAtual = tabuleiro.ObterJogadorAtual();
+                char[,] board = tabuleiro.ObterTabuleiro();
+                ExibirTabuleiro(board);
+                Console.WriteLine($"Vez do jogador {jogadorAtual}");
+                Console.Write("Digite a linha: ");
+                int linha = int.Parse(Console.ReadLine());
+                Console.Write("Digite a coluna: ");
+                int coluna = int.Parse(Console.ReadLine());
+                if (!tabuleiro.FazerJogada(linha, coluna))
+                {
+                    Console.WriteLine("Jogada inválida! Tente novamente.");
+                }
+            }
+            ExibirTabuleiro(tabuleiro.ObterTabuleiro());
+            if (tabuleiro.VerificarVitoria())
+            {
+                Console.WriteLine($"Jogador {tabuleiro.ObterJogadorAtual()} venceu!");
+            }
+            else
+            {
+                Console.WriteLine("Empate!");
+            }
+        }
+        static void ExibirTabuleiro(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j]);
+                    if (j < 2)
+                    {
+                        Console.Write(" | ");
+                    }
+                }
+                Console.WriteLine();
+                if (i < 2)
+                {
+                    Console.WriteLine("---------");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
